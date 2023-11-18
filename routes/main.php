6d53d6b546e5923514ebca5bbfc7573c0d500d27
Dvalyama <91587681+Dvalyama@users.noreply.github.com>
2023-11-18 16:28:34 +0200

    mid_finish

--- routes/main.php
+++ routes/main.php
@@ -1,25 +1,31 @@
 <?php
 
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\Posts\CommentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Posts\CommentController;
 
-Route::view('/', 'welcome');
-Route::redirect('/home', '/');
-Route::get('/test', TestController::class);
+
+Route::view('/', 'welcome')->name('home');
+
+Route::redirect('/home', '/')->name('home.redirect');
 
 
+Route::get('/test', TestController::class)->name('test')->middleware('token:secret'); 
+
+Route::middleware('guest')->group(function (){
 Route::get('register', [RegisterController::class, 'index'])->name('register.index');
 Route::post('register', [RegisterController::class, 'store'])->name('register.store');
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::post('login', [LoginController::class, 'store'])->name('login.store');
+}); 
 
-Route::get('blog', [\App\Http\Controllers\User\BlogController::class, 'index'])->name('blog');
-Route::get('blog/{post}', [\App\Http\Controllers\User\BlogController::class, 'show'])->name('blog.show');
-Route::post('blog/{post}/like', [\App\Http\Controllers\User\BlogController::class, 'like'])->name('blog.like');
+Route::get('blog', [BlogController::class, 'index'])->name('blog');
+Route::get('blog/{post}', [BlogController::class, 'show'])->name('blog.show');
+Route::post('blog/{post}/like', [BlogController::class, 'like'])->name('blog.like');
 
 Route::resource('posts/{post}/comments', CommentController::class);
 
